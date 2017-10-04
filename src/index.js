2236ae28600eb9e39cf7554ef7f1c9e970098660
Alejandro Rosa Pujazon <alejandro.rosa.pujazon@ericsson.com>
2017-10-04 17:07:03 +0200

    First version

---
+++ src/index.js
@@ -0,0 +1,46 @@
+import _ from 'lodash'
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search-bar';
+import VideoList from './components/video-list';
+import VideoDetail from './components/video-detail';
+
+const API_KEY = 'AIzaSyAmoALbRENDA0lDkTODj0osb3OlzJjyjxE';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        
+          this.state = {
+              videos: [], 
+              selectedVideo: null
+          };
+
+          this.videoSearch('tomate');
+    }
+
+    videoSearch(term) {
+        YTSearch({ key: API_KEY, term: term }, videos => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+        });
+    }
+
+    render() {
+        const videoSearch = _.debounce(term => { this.videoSearch(term) }, 300); // Throttle down calls
+        return (
+            <div>
+                <SearchBar onSearchTermChange={videoSearch}/>
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList
+                  videos={this.state.videos}
+                  onVideoSelect={selectedVideo => this.setState({selectedVideo})} />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
